d1bc154cb1cc049ebb694d191d909c81df9ebde7
sinsovannmony <45727570+sinsovannmony@users.noreply.github.com>
2019-06-12 01:07:31 +0700

    Add files via upload

---
+++ assigment10.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+main()
+{
+	int a,b,c;
+	float deta,x1,x2;
+	
+	printf("ax^2+bx+c=0\n");
+	printf("a,b,c are cofficient\n");
+	printf("a=");
+	scanf("%d",a);
+	printf("b=");
+	scanf("%d",b);
+	printf("c=");
+	scanf("%d",c);
+	
+	
+	
+}
+
